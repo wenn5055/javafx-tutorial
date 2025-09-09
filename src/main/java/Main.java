3324e7e49cc39ee18bb9ed9ab4d23bd98fd7b985
wendy <wendytangxd@gmail.com>
2025-09-09 20:35:19 +0800

    Style the Controls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-
+import javafx.scene.layout.Region;
 
 public class Main extends Application {
     private ScrollPane scrollPane;
@@ -39,6 +39,36 @@ public class Main extends Application {
         
         stage.setScene(scene);
         stage.show();
+        
+        //Formatting the window to look as expected
+        
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+        
+        mainLayout.setPrefSize(400.0, 600.0);
+        
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+        
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        
+        userInput.setPrefWidth(325.0);
+        
+        sendButton.setPrefWidth(55.0);
+        
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
         
         //More code to be added here later
     }
