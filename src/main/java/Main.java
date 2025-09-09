66e363c501b30ef1852e96ceda9e185f1aa99331
wendy <wendytangxd@gmail.com>
2025-09-09 21:17:47 +0800

    Add events listening for enter, click button and auto scroll

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,9 +29,6 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
         
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
-        
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
         
@@ -70,6 +67,26 @@ public class Main extends Application {
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
         
-        //More code to be added here later
+        //Handling user input
+        
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+        
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+        
+    }
+    
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        dialogContainer.getChildren().addAll(new DialogBox(userInput.getText(), userImage));
+        userInput.clear();
     }
 }
