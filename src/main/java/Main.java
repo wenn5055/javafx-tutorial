af8d83db4562e83e441dbe1fe4fd9b78c6ba494a
wendy <wendytangxd@gmail.com>
2025-09-09 20:31:34 +0800

    Stage 2: Add nodes (elements in the window)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,45 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 
 public class Main extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
     
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //Setting up required components
         
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+        
+        userInput = new TextField();
+        sendButton = new Button("Send");
+        
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+        
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        
+        scene = new Scene(mainLayout);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+        //More code to be added here later
     }
 }
